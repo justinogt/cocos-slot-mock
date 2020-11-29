09ade745e7d726b79b2c75eafb09bd6409c8944c
Jorge Justino <jorge.justino@bludata.com.br>
2020-11-29 20:44:28 -0300

    Added tile win flow

--- SlotMock/assets/scripts/slots/Tile.ts
+++ SlotMock/assets/scripts/slots/Tile.ts
@@ -5,9 +5,19 @@ const { ccclass } = cc._decorator;
 @ccclass
 export default class Tile extends cc.Component {
   private sprite: cc.Sprite;
+  private glow: cc.Node;
 
   start() {
     this.sprite = this.node.getComponent(cc.Sprite);
+    this.glow = this.node.getChildByName('glow');
+    this.setRandom();
+  }
+
+  setWinner() {
+    this.glow.active = true;
+  }
+  setNormal() {
+    this.glow.active = false;
   }
 
   setTile(index: number) {
@@ -15,6 +25,7 @@ export default class Tile extends cc.Component {
   }
 
   setRandom() {
+    console.log('dsnadsla');
     this.sprite.spriteFrame = CharactersResource.instance.getRandom();
   }
 }
